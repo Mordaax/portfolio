73e85e8de4be6889a2222519ecc1aa8a63bd9b00
Mordaax <53942938+Mordaax@users.noreply.github.com>
2023-03-21 16:37:28 +0800

    imported fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import "@/assets/css/vendor/bootstrap.min.css"
-import "@/assets/css/vendor/slick.css"
-import "@/assets/css/vendor/slick-theme.css"
-import "@/assets/css/vendor/aos.css"
-import "@/assets/css/plugins/feature.css"
-import "@/assets/css/style.css"
+import "@/public/assets/css/vendor/bootstrap.min.css"
+import "@/public/assets/css/vendor/slick.css"
+import "@/public/assets/css/vendor/slick-theme.css"
+import "@/public/assets/css/vendor/aos.css"
+import "@/public/assets/css/plugins/feature.css"
+import "@/public/assets/css/style.css"
 //import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
